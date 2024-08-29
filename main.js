bbd4b1819c7560bc3da1d61e8d507ce40243ae34
Tarps240 <sl3966@gmail.com>
2024-08-29 16:48:49 -0700

    Initialize repro

---
+++ main.js
@@ -0,0 +1,107 @@
+/******************
+ * YOUR CODE HERE *
+ ******************/
+//xify problem.
+function xify(inputString) {
+  let result = ''; // Initialize an empty string to hold the result
+  
+  // Loop through each character in the input string
+  for (let i = 0; i < inputString.length; i++) {
+      result += 'x'; // Append 'x' to the result for each character
+  }
+  
+  return result; // Return the resulting string
+}
+
+const originalString = "Hello World!";
+const replacedString = xify(originalString);
+
+console.log(replacedString);
+
+// yellingChars excersize.
+
+
+
+ /********************************************
+ * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
+ *           PLEASE DON'T TOUCH!            *
+ ********************************************/
+
+if (typeof xify === 'undefined') {
+  xify = undefined;
+}
+
+if (typeof smilify === 'undefined') {
+  smilify = undefined;
+}
+
+if (typeof yellingChars === 'undefined') {
+  yellingChars = undefined;
+}
+
+if (typeof numberedChars === 'undefined') {
+  numberedChars = undefined;
+}
+
+if (typeof indexedChars === 'undefined') {
+  indexedChars = undefined;
+}
+
+if (typeof exclaim === 'undefined') {
+  exclaim = undefined;
+}
+
+if (typeof repeatIt === 'undefined') {
+  repeatIt = undefined;
+}
+
+if (typeof truncate === 'undefined') {
+  truncate = undefined;
+}
+
+if (typeof ciEmailify === 'undefined') {
+  ciEmailify = undefined;
+}
+
+if (typeof reverse === 'undefined') {
+  reverse = undefined;
+}
+
+if (typeof onlyVowels === 'undefined') {
+  onlyVowels = undefined;
+}
+
+if (typeof crazyCase === 'undefined') {
+  crazyCase = undefined;
+}
+
+if (typeof titleCase === 'undefined') {
+  titleCase = undefined;
+}
+
+if (typeof camelCase === 'undefined') {
+  camelCase = undefined;
+}
+
+if (typeof crazyCase2ReturnOfCrazyCase === 'undefined') {
+  crazyCase2ReturnOfCrazyCase = undefined;
+}
+
+
+module.exports = {
+  xify,
+  smilify,
+  indexedChars,
+  yellingChars, // <-- add this line here (no need for the comment, obvs)
+  numberedChars,
+  exclaim,
+  repeatIt,
+  truncate,
+  ciEmailify,
+  reverse,
+  onlyVowels,
+  crazyCase,
+  titleCase,
+  camelCase,
+  crazyCase2ReturnOfCrazyCase,
+}
