9a37568c2fd88a7840b5ba1a9eab85eeb5989af0
Tarps240 <sl3966@gmail.com>
2024-08-30 01:04:26 -0700

    halfway done

--- main.js
+++ main.js
@@ -3,24 +3,102 @@
  ******************/
 //xify problem.
 function xify(inputString) {
-  let result = ''; // Initialize an empty string to hold the result
+
+  let result = '';
   
   // Loop through each character in the input string
   for (let i = 0; i < inputString.length; i++) {
       result += 'x'; // Append 'x' to the result for each character
   }
   
-  return result; // Return the resulting string
+  return result;
 }
 
-const originalString = "Hello World!";
+const originalString = "I love Renee!";
 const replacedString = xify(originalString);
 
 console.log(replacedString);
 
 // yellingChars excersize.
 
+function yellingChars(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += str[i] + '!';
+  }
+  return result;
+}
 
+console.log(yellingChars("I love Renee!"));
+
+// indexedChars problem.
+
+function indexedChars(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+      result += i + str[i];
+  }
+  return result;
+}
+
+console.log(indexedChars("I wanna go fast!"));
+
+// numberedChars problem.
+function numberedChars(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += '(' + (i + 1) + ')' + str[i];
+  }
+  return result;
+}
+console.log(numberedChars("Testing the function"));
+
+// exclaim problem.
+function exclaim(sentence) {
+  let result = '';
+  for (let i = 0; i < sentence.length; i++) {
+      if (sentence[i] === '?' || sentence[i] === '.') {
+          result += '!';
+      } else {
+          result += sentence[i];
+      }
+  }
+  return result;
+}
+
+console.log(exclaim('What are you doing? Are you a fool?')); 
+console.log(exclaim('This is fine.'));
+
+// repeatIt problem.
+
+function repeatIt(str, n) {
+  let result = '';
+  for (let i = 0; i < n; i++) {
+      result += str;
+  }
+  return result;
+}
+
+console.log(repeatIt("hello", 3));
+console.log(repeatIt("abc", 2));
+
+// truncate problem.
+function truncate(str) {
+  let result = '';
+  let maxLength = 15;
+
+  for (let i = 0; i < maxLength && i < str.length; i++) {
+      result += str[i];
+  }
+
+  if (str.length > 18) {
+      result += '...';
+  }
+
+  return result;
+}
+
+console.log(truncate('The fault, dear Brutus, is not in our stars, but in ourselves.')); 
 
  /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
@@ -31,9 +109,9 @@ if (typeof xify === 'undefined') {
   xify = undefined;
 }
 
-if (typeof smilify === 'undefined') {
-  smilify = undefined;
-}
+// if (typeof smilify === 'undefined') {
+//   smilify = undefined;
+// }
 
 if (typeof yellingChars === 'undefined') {
   yellingChars = undefined;
@@ -90,7 +168,6 @@ if (typeof crazyCase2ReturnOfCrazyCase === 'undefined') {
 
 module.exports = {
   xify,
-  smilify,
   indexedChars,
   yellingChars, // <-- add this line here (no need for the comment, obvs)
   numberedChars,
